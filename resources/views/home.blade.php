f9c7624414576cb8d4c1f9595b6ff074e93e0007
vingoquoc <vingoquoc@gmail.com>
2024-08-02 13:42:55 +0700

    check git

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
       <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
           <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">minagslk@gmail.com</a></i>
-          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+60 12 341 0788</span></i>
+          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+60 12 341 0788888888</span></i>
         </div>
         <div class="social-links d-none d-md-flex align-items-center">
           <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
