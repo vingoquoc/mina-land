d2e1341b767cd94b7976886a05d2fce35e999814
vingoquoc <vingoquoc@gmail.com>
2024-08-02 15:49:36 +0700

    modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
     <div class="branding d-flex align-items-cente">
 
       <div class="container position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="/" class="logo d-flex align-items-center">
           <!-- Uncomment the line below if you also wish to use an image logo -->
           <!-- <img src="resources/img/logo.png" alt=""> -->
           <h1 class="sitename">Mina Land</h1>
