038891c082e6743199900d031da22accdf807bbb
vingoquoc <vingoquoc@gmail.com>
2024-08-02 09:21:30 +0700

    change text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,7 +93,7 @@
           <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
             <div class="service-item position-relative">
               <div class="icon"><i class="bi bi-activity icon"></i></div>
-              <h4><a href="" class="stretched-link">Lorem Ipsum</a></h4>
+              <h4><a href="" class="stretched-link">Love Mina</a></h4>
               <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
             </div>
           </div><!-- End Service Item -->
